7512d8b1f1180dd8aa92ad6c5d567b722c3ca9ee
laiminhkiet7522 <laiminhkiet07052002@gmail.com>
2024-07-05 15:17:14 +0700

    Buoi 33

---
+++ unica_buoi_33/main.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *pNext;
+};
+Node *initNode(int value)
+{
+    Node *p = new Node();
+    p->data=value;
+    p->pNext=NULL;
+    return p;
+}
+struct Stack
+{
+    Node *pTop;
+};
+void initStack(Stack &s)
+{
+    s.pTop=NULL;
+}
+void push(Stack &s, Node *p)
+{
+    if(s.pTop==NULL)
+    {
+        s.pTop=p;
+    }
+    else
+    {
+        p->pNext=s.pTop;
+        s.pTop=p;
+    }
+}
+void printStack(Stack s)
+{
+    cout << "Stack = Top < ";
+    for(Node* p = s.pTop; p != NULL; p=p->pNext)
+    {
+        cout << p->data << " ";
+    }
+    cout << ">" << endl;
+}
+int main()
+{
+    Stack s;
+    initStack(s);
+    Node *p1 = initNode(10);
+    Node *p2 = initNode(20);
+    Node *p3 = initNode(30);
+
+    push(s,p1);
+    push(s,p2);
+    push(s,p3);
+
+    printStack(s);
+    return 0;
+}
