5a8b6e28ff6cc90bbf54f8356367c57b2ed4449f
laiminhkiet7522 <laiminhkiet07052002@gmail.com>
2024-07-12 10:43:26 +0700

    Buoi 46

---
+++ unica_buoi_46/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *pNext;
+};
+Node *initNode(int value)
+{
+    Node *p = new Node();
+    if(p==NULL)
+    {
+        cout<<"Cap phat bo nho khong thanh cong";
+        return NULL;
+    }
+    p->data=value;
+    p->pNext=NULL;
+    return p;
+}
+struct Queue
+{
+    Node *pFront;
+    Node *pRear;
+};
+void initQueue(Queue &q)
+{
+    q.pFront=q.pRear=NULL;
+}
+void enQueue(Queue &q, int value)
+{
+    Node *p = initNode(value);
+    if(q.pFront==NULL)
+    {
+        q.pFront=q.pRear=p;
+    }
+    else
+    {
+        q.pRear->pNext=p;
+        q.pRear.=p;
+    }
+}
+int main()
+{
+    Queue q;
+    initQueue(q);
+    return 0;
+}
